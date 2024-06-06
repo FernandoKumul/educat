da5e72586050b732d61bc58ba7379bb874f51545
Luis Flores <luisgomez7829@gmail.com>
2024-06-06 14:24:41 -0500

    feat: Update Cards component to display course information in CourseSearch page

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,22 @@
-const Cards = () => {
+interface CardsProps {
+    title: string;
+    instructor: string;
+    price: string;
+    score: number;
+}
+const Cards = ({title, instructor, price, score}: CardsProps) => {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="size-[280px]">
+            <div className="bg-gradient-to-r from-purple-500 via-violet-600 to-indigo-400 h-[160px] rounded-lg">
+                <div className="bg-white text-neutral-950 rounded-tl-lg rounded-br-lg w-[60px] flex justify-center">
+                    <p>{score}</p>
+                </div>
+            </div>
+            <div>
+                <h2 className="text-2xl font-bold">{title}</h2>
+                <p className="text-sm">{instructor}</p>
+                <p className="text-sm">{price}</p>
+            </div>
         </div>
     );
 }
--- src/pages/CourseSearch.tsx
+++ src/pages/CourseSearch.tsx
@@ -2,9 +2,18 @@ import Cards from "../components/Cards";
 
 const CourseSearch = () => {
     return (
-        <div>
-            < Cards />
-            
+        <div className="flex items-center flex-col">
+            <h1 className=" text-3xl m-5 ">Resultados de busqueda</h1>
+            <section className="mt-5 inline-grid grid-cols-1 xl:grid-cols-4 gap-10">
+                <Cards title="Titulo" instructor="Instructor" price="$27" score={4.7} />
+                <Cards title="Titulo" instructor="Instructor" price="$27" score={4.7} />
+                <Cards title="Titulo" instructor="Instructor" price="$27" score={4.7} />
+                <Cards title="Titulo" instructor="Instructor" price="$27" score={4.7} />
+                <Cards title="Titulo" instructor="Instructor" price="$27" score={4.7} />
+                <Cards title="Titulo" instructor="Instructor" price="$27" score={4.7} />
+                <Cards title="Titulo" instructor="Instructor" price="$27" score={4.7} />
+                <Cards title="Titulo" instructor="Instructor" price="$27" score={4.7} />
+            </section>
         </div>
     );
 }
