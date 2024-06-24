47cc7177e2b080f96dcd957bce013fb9313b707f
FernandoKumul <ferdesarrollador11@outlook.com>
2024-06-24 02:13:17 -0500

    Course Page

---
+++ src/data/DifficultyData.ts
@@ -0,0 +1,8 @@
+const DifficultyData = [
+  { id: 1, name: 'easy', color: 'bg-[#86efac]', text: 'Fácil' },
+  { id: 2, name: 'normal', color: 'bg-[#FCE35F]', text: 'Normal' },
+  { id: 3, name: 'hard', color: 'bg-[#FFC385]', text: 'Difícil' },
+  { id: 4, name: 'expert', color: 'bg-[#fca5a5]', text: 'Experto' },
+];
+
+export default DifficultyData
