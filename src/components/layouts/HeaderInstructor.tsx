a8ef60693f35de486efeb8dca278c3c7ba8bcfb2
Luis Flores <luisgomez7829@gmail.com>
2024-07-06 01:13:57 -0500

    feat: Update InstructorLayout and related components for responsive design and sidebar functionality

--- src/components/layouts/HeaderInstructor.tsx
+++ src/components/layouts/HeaderInstructor.tsx
@@ -1,13 +1,17 @@
 import { RiMenuLine, RiUserLine } from "@remixicon/react";
 import { Link } from "react-router-dom";
 
-const HeaderInstructor = () => {
+type HeaderInstructorProps = {
+  handleSidebar: () => void;
+};
+
+const HeaderInstructor = ({handleSidebar}: HeaderInstructorProps) => {
   return (
     <header className="flex bg-header justify-end">
       <Link to={'/instructor/profile'}>
         <RiUserLine className="size-10 m-3 mr-6 max-sm:hidden" />
       </Link>
-      <RiMenuLine className="size-7 m-3 mr-6 hidden max-sm:block" />
+      <RiMenuLine onClick={handleSidebar} className="size-7 m-3 mr-6 hidden max-sm:block" />
     </header>
   );
 }
--- src/components/layouts/InstructorSidebar.tsx
+++ src/components/layouts/InstructorSidebar.tsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import { useEffect, useState } from "react";
 
-const InstructorSidebar = () => {
+type InstructorSidebarProps = {
+  openSidebar: boolean;
+  handleSidebar: () => void;
+};
+
+const InstructorSidebar = ({handleSidebar, openSidebar}: InstructorSidebarProps) => {
   const [path, setPath] = useState('');
   const currentView = (pathName : string) => {
     setPath(pathName);
@@ -13,8 +18,9 @@ const InstructorSidebar = () => {
     setPath(window.location.pathname.split('/')[2]);
   }, [path]);
   return (
-    <aside className="min-w-56 flex-shrink-0 bg-black-1 h-screen hidden lg:block">
-      <div className="flex flex-col py-5 px-10 gap-y-5">
+    <aside className={openSidebar ? 'h-screen max-sm:flex max-sm:w-screen max-sm:absolute max-sm:z-10 max-sm:right-0 lg:min-w-56 lg:flex-shrink-0 lg:block' : 'hidden bg-black-1 h-screen lg:min-w-56 lg:flex-shrink-0 lg:block'}>
+      <div onClick={handleSidebar} className={openSidebar ? 'max-sm:block max-sm:h-screen max-sm:w-1/5 max-sm:bg-black max-sm:opacity-50': 'hidden'}></div>
+      <div className="max-sm:w-4/5 bg-black-1 flex flex-col py-5 px-10 gap-y-5">
         <Link to={'/'} className="flex items-center justify-center mb-5">
           <img src={logo} alt="logo educat" className="w-44"/>
         </Link>
