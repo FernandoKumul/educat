8ae9895f2500cb89f0a44401e56b4a8b990a8aa8
FernandoKumul <ferdesarrollador11@outlook.com>
2024-07-11 13:56:07 -0500

    Show items and price total

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,9 +1,14 @@
 import PayPalservice from "../services/PayPalService";
 import { OnApproveBraintreeActions, OnApproveBraintreeData, PayPalButtons, PayPalScriptProvider, ReactPayPalScriptOptions } from "@paypal/react-paypal-js";
 import { AxiosError } from "axios";
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import CartContext from "../contexts/CartContext";
+import { ICartItemCourse } from "../interfaces/ICartItemCourse";
+import CartService from "../services/CartService";
+import { RiLoader4Line } from "@remixicon/react";
+import { useNavigate } from "react-router-dom";
+import { CurrencyFormat } from "../utils/CurrencyUtils";
 
 
 const CheckoutPage = () => {
@@ -17,6 +22,12 @@ const CheckoutPage = () => {
   }
 
   const { setItems } = useContext(CartContext)
+  const [isLoading, setLoading] = useState(true)
+  const [isCartItems, setCartItems] = useState<ICartItemCourse[]>([])
+  const dataFetch = useRef<boolean>(false)
+  const navigate = useNavigate()
+
+
 
   const handleCreateOrder = async () => {
     try {
@@ -77,20 +88,88 @@ const CheckoutPage = () => {
     }
   }
 
+  const getItems = async () => {
+    try {
+      dataFetch.current = true
+      setLoading(true)
+      const items = await CartService.getUserCart()
+      if(items.length === 0) {
+        toast.info("No tienes nigún producto en el carrito para comprar")
+        navigate("/")
+      }
+      setCartItems(items)
+      setItems(items)
+    } catch (error) {
+      console.log(error)
+      setCartItems([])
+      setItems([])
+      if (error instanceof AxiosError) {
+        if (error.response?.data.message) {
+          return toast.error(error.response?.data.message);
+        }
+
+        return toast.error('Oops... Ocurrió un error, Inténtelo más tarde');
+      }
+    } finally {
+      setLoading(false)
+    }
+  };
+
+  const totalPrice = () => {
+    let count = 0
+    for (const item of isCartItems) {
+      count += item.course.price ?? 0
+    }
+    return count
+  }
+
+  useEffect(() => {
+    if(!dataFetch.current) {
+      getItems()
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="flex-grow flex items-center justify-center">
+        <RiLoader4Line size={48} className="animate-spin" />
+      </div>
+    )
+  }
+
   return (
-    <section>
-      <h2>Checkout</h2>
-      <PayPalScriptProvider options={initialOptions}>
-        <PayPalButtons
-          style={{
-            shape: "rect",
-            color: 'black',
-            layout: "vertical",
-          }}
-          createOrder={handleCreateOrder}
-          onApprove={(data, actions) => handleCaptureOrder(data as OnApproveBraintreeData, actions as OnApproveBraintreeActions)}
-        />
-      </PayPalScriptProvider>
+    <section className="grid gap-12 grid-cols-[5fr_2fr] py-8 px-16">
+      <article className="bg-[#25202A] rounded-md py-10 px-8">
+        <h2 className="text-2xl font-semibold mb-4">Cursos de la compra</h2>
+        {isCartItems.map(item => (
+          <article key={item.pkCartWishList} className="flex gap-2">
+            <h4>{item.course.title}</h4>
+            <p>{CurrencyFormat(item.course.price ?? 0)}</p>
+          </article>
+        ))}
+      </article>
+      <article className="bg-[#25202A] rounded-md py-10 px-8">
+        <h2 className="text-center text-2xl font-semibold  mb-4">Resumen</h2>
+        <div className="flex justify-between">
+          <p>Precio: </p><p>{CurrencyFormat(totalPrice())}</p>
+        </div>
+        <div className="h-[1px] bg-[#787081] my-6"></div>
+        <div className="flex justify-between mb-6">
+          <p>Total: </p><p>{CurrencyFormat(totalPrice())}</p>
+        </div>
+        <PayPalScriptProvider options={initialOptions}>
+          <PayPalButtons
+            style={{
+              shape: "rect",
+              color: 'black',
+              layout: "vertical",
+            }}
+            createOrder={handleCreateOrder}
+            onApprove={(data, actions) => handleCaptureOrder(data as OnApproveBraintreeData, actions as OnApproveBraintreeActions)}
+          />
+        </PayPalScriptProvider>
+      </article>
     </section>
   );
 }
