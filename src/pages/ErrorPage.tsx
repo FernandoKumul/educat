d930390d795375a448dec702cc9857e571507fe3
FernandoKumul <ferdesarrollador11@outlook.com>
2024-07-15 15:52:39 -0500

    Optimize image size

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,7 +4,7 @@ const ErrorPage = () => {
     <div className="flex flex-grow justify-center items-center flex-col min-h-screen">
       <h1>Oops!</h1>
       <p className="mb-2">{"No se encontró la página solicitada (っ °Д °;)っ."}</p>
-      <img className="w-1/3" src="https://cataas.com/cat?tags=scared,nice" alt="cataas-img" />
+      <img className="w-1/3" src="https://cataas.com/cat?tags=scared,nice&width=400" alt="cataas-img" />
     </div>
   );
 }
