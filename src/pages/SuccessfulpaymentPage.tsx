20beebb36988f67f49b1bf6d18110cd2b317a2c2
FernandoKumul <ferdesarrollador11@outlook.com>
2024-07-17 05:22:10 -0500

    Change color icon in success payment

--- src/pages/SuccessfulpaymentPage.tsx
+++ src/pages/SuccessfulpaymentPage.tsx
@@ -6,7 +6,7 @@ const SuccessPaymentPage = () => {
   return (
     <main className="flex min-h-screen justify-center items-center px-[5%]">
       <article className="px-4 py-6 bg-black-auth flex flex-col  items-center rounded-lg max-w-[440px] md:px-6">
-        <RiCheckboxCircleLine size={48} />
+        <RiCheckboxCircleLine size={60} className="text-green-500" />
         <h2 className="font-medium text-xl sm:text-[22px] mt-2 mb-1">¡Pago Exitoso!</h2>
         <p className="text-secundary-text text-center text-[15px] mb-4 text-balance w-full">
           Gracias por tu compra, es hora de aprender.
