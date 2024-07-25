7f426309e42945fa417c7fb225c93c5138ab893d
FernandoKumul <ferdesarrollador11@outlook.com>
2024-07-25 18:27:13 -0500

    change url in service course search

--- src/services/CourseSearchService.ts
+++ src/services/CourseSearchService.ts
@@ -6,12 +6,12 @@ const BASE_URL =  import.meta.env.VITE_URL_API ?? 'https://localhost:7245/api'
 
 async function search(pageNumber: number, query: string, category: string = 'all') {
     if (query === '') {
-        const response = await axios.get(`${BASE_URL}CourseSearch/search?PageNumber=${pageNumber}&PageSize=12&category=${category}`);
+        const response = await axios.get(`${BASE_URL}/CourseSearch/search?PageNumber=${pageNumber}&PageSize=12&category=${category}`);
         const courses: ICourseSearch[] = response.data.data.result;
         const count: number = response.data.data.count;
         return { courses, count };
     } else {
-        const response = await axios.get(`${BASE_URL}CourseSearch/search?PageNumber=${pageNumber}&PageSize=12&query=${query}&category=${category}`);
+        const response = await axios.get(`${BASE_URL}/CourseSearch/search?PageNumber=${pageNumber}&PageSize=12&query=${query}&category=${category}`);
         const courses: ICourseSearch[] = response.data.data.result;
         const count: number = response.data.data.count;
         return { courses, count };
